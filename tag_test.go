7dc5e8ffbebb030521c0191d74f7b17dc8cd5ca4
Alec Thomas <alec@swapoff.org>
2016-02-15 14:10:52 +1100

    Data-driven tests for tag parser.

--- tag_test.go
+++ tag_test.go
@@ -6,80 +6,79 @@ import (
 	"github.com/alecthomas/assert"
 )
 
-func TestParseTagSequence(t *testing.T) {
-	actual := parseTag(`"a" "b"`)
-	expected := expression{
-		alternative{str("a"), str("b")},
-	}
-	assert.Equal(t, expected, actual)
-}
-
-func TestParseTagRange(t *testing.T) {
-	actual := parseTag(`"a" … "b"`)
-	expected := expression{
-		alternative{srange{str("a"), str("b")}},
-	}
-	assert.Equal(t, expected, actual)
-}
-
-func TestParseTagAlternative(t *testing.T) {
-	actual := parseTag(`"a" | "b"`)
-	expected := expression{
-		alternative{str("a")},
-		alternative{str("b")},
-	}
-	assert.Equal(t, expected, actual)
-}
-
-func TestParseTagOptional(t *testing.T) {
-	actual := parseTag(`[ "a" ]`)
-	expected := expression{alternative{
-		optional{{str("a")}},
-	}}
-	assert.Equal(t, expected, actual)
-}
-
-func TestParseTagGroup(t *testing.T) {
-	actual := parseTag(`( "a" @ )`)
-	expected := expression{
-		alternative{
-			group{
-				alternative{str("a"), self{}},
+var tagParserTestData = []struct {
+	actual   string
+	expected expression
+}{
+	{
+		`"a" "b"`,
+		expression{
+			alternative{str("a"), str("b")},
+		},
+	},
+	{
+		`"a" … "b"`,
+		expression{
+			alternative{srange{str("a"), str("b")}},
+		},
+	},
+	{
+		`"a" | "b"`,
+		expression{
+			alternative{str("a")},
+			alternative{str("b")},
+		},
+	},
+	{
+		`[ "a" ]`,
+		expression{
+			alternative{optional{{str("a")}}},
+		},
+	},
+	{
+		`( "a" @ )`,
+		expression{
+			alternative{
+				group{
+					alternative{str("a"), self{}},
+				},
 			},
 		},
-	}
-	assert.Equal(t, expected, actual)
-}
-
-func TestParseTagRepitition(t *testing.T) {
-	actual := parseTag(`{ "a" | @ }`)
-	expected := expression{
-		alternative{
-			repitition{
-				alternative{str("a")},
-				alternative{self{}},
+	},
+	{
+		`{ "a" | @ }`,
+		expression{
+			alternative{
+				repitition{
+					alternative{str("a")},
+					alternative{self{}},
+				},
 			},
 		},
-	}
-	assert.Equal(t, expected, actual)
-}
-
-func TestParseComplexTag(t *testing.T) {
-	actual := parseTag(`("a"…"z" | "A"…"Z" | "_") {"a"…"z" | "A"…"Z" | "0"…"9" | "_"}`)
-	expected := expression{
-		alternative{
-			group{
-				alternative{srange{start: str("a"), end: str("z")}},
-				alternative{srange{start: str("A"), end: str("Z")}},
-				alternative{str("_")},
-			},
-			repitition{
-				alternative{srange{start: str("a"), end: str("z")}},
-				alternative{srange{start: str("A"), end: str("Z")}},
-				alternative{srange{start: str("0"), end: str("9")}},
-				alternative{str("_")},
+	},
+	{
+		`("a"…"z" | "A"…"Z" | "_") {"a"…"z" | "A"…"Z" | "0"…"9" | "_"}`,
+		expression{
+			alternative{
+				group{
+					alternative{srange{start: str("a"), end: str("z")}},
+					alternative{srange{start: str("A"), end: str("Z")}},
+					alternative{str("_")},
+				},
+				repitition{
+					alternative{srange{start: str("a"), end: str("z")}},
+					alternative{srange{start: str("A"), end: str("Z")}},
+					alternative{srange{start: str("0"), end: str("9")}},
+					alternative{str("_")},
+				},
 			},
 		},
+	},
+}
+
+func TestTagParser(t *testing.T) {
+	for _, data := range tagParserTestData {
+		actual := parseTag(data.actual)
+		assert.Equal(t, data.expected, actual)
 	}
-	assert.Equal(t, expected, actual)
 }
