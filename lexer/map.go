ff93e891689221b8a57253624ce4af26ebab8ccc
Alec Thomas <alec@swapoff.org>
2017-02-02 23:29:53 -0800

    Consolidate mappers and add tests.

--- lexer/map.go
+++ lexer/map.go
@@ -1,6 +1,9 @@
 package lexer
 
-import "io"
+import (
+	"io"
+	"strconv"
+)
 
 type mapperDef struct {
 	def Definition
@@ -43,4 +46,44 @@ func (m *mapper) Next() Token {
 	t := m.Peek()
 	m.peek = nil
 	return t
+}
+
+func MakeSymbolTable(def Definition, types ...string) map[rune]bool {
+	sym := def.Symbols()
+	table := map[rune]bool{}
+	for _, r := range types {
+		table[sym[r]] = true
+	}
+	return table
+}
+
+// Elide wraps a Lexer, removing tokens matching the given types.
+func Elide(def Definition, types ...string) Definition {
+	table := MakeSymbolTable(def, types...)
+	return Map(def, func(token *Token) *Token {
+		if table[token.Type] {
+			return nil
+		}
+		return token
+	})
+}
+
+// Unquote applies strconv.Unquote() to tokens of the given types.
+//
+// Tokens of type "String" will be unquoted if no other types are provided.
+func Unquote(def Definition, types ...string) Definition {
+	if len(types) == 0 {
+		types = []string{"String"}
+	}
+	table := MakeSymbolTable(def, types...)
+	return Map(def, func(t *Token) *Token {
+		if table[t.Type] {
+			value, err := strconv.Unquote(t.Value)
+			if err != nil {
+				Panicf(t.Pos, "invalid quoted string %q: %s", t.Value, err.Error())
+			}
+			t.Value = value
+		}
+		return t
+	})
 }
--- lexer/map_test.go
+++ lexer/map_test.go
@@ -31,3 +31,29 @@ func TestMap(t *testing.T) {
 
 	require.Equal(t, expected, actual)
 }
+
+func TestUnquote(t *testing.T) {
+	def := Unquote(Must(Regexp(`(\s+)|(?P<Ident>\w+)|(?P<String>"[^"]+")`)))
+	lexer := def.Lex(strings.NewReader(`hello "world"`))
+	actual, err := ConsumeAll(lexer)
+	require.NoError(t, err)
+	expected := []Token{
+		Token{Type: -3, Value: "hello", Pos: Position{Filename: "", Offset: 0, Line: 1, Column: 1}},
+		Token{Type: -4, Value: "world", Pos: Position{Filename: "", Offset: 6, Line: 1, Column: 7}},
+		Token{Type: -1, Value: "<<EOF>>", Pos: Position{Filename: "", Offset: 13, Line: 1, Column: 14}},
+	}
+	require.Equal(t, expected, actual)
+}
+
+func TestElide(t *testing.T) {
+	def := Elide(Must(Regexp(`(?P<Whitespace>\s+)|(?P<Ident>\w+)`)), "Whitespace")
+	lex := def.Lex(strings.NewReader("hello world"))
+	actual, err := ConsumeAll(lex)
+	require.NoError(t, err)
+	expected := []Token{
+		Token{Type: -3, Value: "hello", Pos: Position{Filename: "", Offset: 0, Line: 1, Column: 1}},
+		Token{Type: -3, Value: "world", Pos: Position{Filename: "", Offset: 6, Line: 1, Column: 7}},
+		Token{Type: -1, Value: "<<EOF>>", Pos: Position{Filename: "", Offset: 11, Line: 1, Column: 12}},
+	}
+	require.Equal(t, expected, actual)
+}
