b7988c50377644d0fcc0450b58753bd33c2a0dc6
Alec Thomas <alec@swapoff.org>
2018-03-05 23:40:42 +1100

    Fix spaces.

--- lexer/peek.go
+++ lexer/peek.go
@@ -2,31 +2,31 @@ package lexer
 
 // Upgrade a SimpleLexer to a Lexer with arbitrary lookahead.
 func Upgrade(simple SimpleLexer) Lexer {
-  return &lookaheadLexer{SimpleLexer: simple}
+	return &lookaheadLexer{SimpleLexer: simple}
 }
 
 type lookaheadLexer struct {
-  SimpleLexer
-  peeked []Token
+	SimpleLexer
+	peeked []Token
 }
 
 func (l *lookaheadLexer) Peek(n int) Token {
-  for len(l.peeked) <= n {
-    t := l.SimpleLexer.Next()
-    if t.EOF() {
-      return t
-    }
-    l.peeked = append(l.peeked, t)
-  }
+	for len(l.peeked) <= n {
+		t := l.SimpleLexer.Next()
+		if t.EOF() {
+			return t
+		}
+		l.peeked = append(l.peeked, t)
+	}
 
-  return l.peeked[n]
+	return l.peeked[n]
 }
 
 func (l *lookaheadLexer) Next() Token {
-  if len(l.peeked) > 0 {
-    t := l.peeked[0]
-    l.peeked = l.peeked[1:]
-    return t
-  }
-  return l.SimpleLexer.Next()
+	if len(l.peeked) > 0 {
+		t := l.peeked[0]
+		l.peeked = l.peeked[1:]
+		return t
+	}
+	return l.SimpleLexer.Next()
 }
--- lexer/peek_test.go
+++ lexer/peek_test.go
@@ -1,32 +1,32 @@
 package lexer
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/gotestyourself/gotestyourself/assert"
+	"github.com/gotestyourself/gotestyourself/assert"
 )
 
 type staticLexer struct {
-  tokens []Token
+	tokens []Token
 }
 
 func (s *staticLexer) Next() Token {
-  if len(s.tokens) == 0 {
-    return EOFToken
-  }
-  t := s.tokens[0]
-  s.tokens = s.tokens[1:]
-  return t
+	if len(s.tokens) == 0 {
+		return EOFToken
+	}
+	t := s.tokens[0]
+	s.tokens = s.tokens[1:]
+	return t
 }
 
 func TestUpgrade(t *testing.T) {
-  t0 := Token{Type: 1, Value: "moo"}
-  t1 := Token{Type: 2, Value: "blah"}
-  l := Upgrade(&staticLexer{tokens: []Token{t0, t1}})
-  assert.Equal(t, t0, l.Peek(0))
-  assert.Equal(t, t0, l.Peek(0))
-  assert.Equal(t, t1, l.Peek(1))
-  assert.Equal(t, t1, l.Peek(1))
-  assert.Equal(t, EOFToken, l.Peek(2))
-  assert.Equal(t, EOFToken, l.Peek(3))
+	t0 := Token{Type: 1, Value: "moo"}
+	t1 := Token{Type: 2, Value: "blah"}
+	l := Upgrade(&staticLexer{tokens: []Token{t0, t1}})
+	assert.Equal(t, t0, l.Peek(0))
+	assert.Equal(t, t0, l.Peek(0))
+	assert.Equal(t, t1, l.Peek(1))
+	assert.Equal(t, t1, l.Peek(1))
+	assert.Equal(t, EOFToken, l.Peek(2))
+	assert.Equal(t, EOFToken, l.Peek(3))
 }
