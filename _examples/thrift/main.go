235af293ef87006029fd94081c4b6999031aa824
Alec Thomas <alec@swapoff.org>
2018-06-15 18:18:41 +1000

    Add positions to Thrift AST.

--- _examples/thrift/main.go
+++ _examples/thrift/main.go
@@ -14,6 +14,7 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/alecthomas/participle"
+	"github.com/alecthomas/participle/lexer"
 	"github.com/alecthomas/repr"
 )
 
@@ -22,22 +23,26 @@ var (
 )
 
 type Namespace struct {
+	Pos       lexer.Position
 	Language  string `"namespace" @Ident`
 	Namespace string `@Ident { @"." @Ident }`
 }
 
 type Type struct {
+	Pos     lexer.Position
 	Name    string `@Ident { @"." @Ident }`
 	TypeOne *Type  `[ "<" @@ [ ","`
 	TypeTwo *Type  `           @@ ] ">" ]`
 }
 
 type Annotation struct {
+	Pos   lexer.Position
 	Key   string   `@Ident { @"." @Ident }`
 	Value *Literal `[ "=" @@ ]`
 }
 
 type Field struct {
+	Pos         lexer.Position
 	ID          string        `@Int ":"`
 	Requirement string        `@[ "optional" | "required" ]`
 	Type        *Type         `@@`
@@ -47,12 +52,14 @@ type Field struct {
 }
 
 type Exception struct {
+	Pos         lexer.Position
 	Name        string        `"exception" @Ident "{"`
 	Fields      []*Field      `@@ { @@ } "}"`
 	Annotations []*Annotation `[ "(" @@ { "," @@ } ")" ]`
 }
 
 type Struct struct {
+	Pos         lexer.Position
 	Union       bool          `( "struct" | @"union" )`
 	Name        string        `@Ident "{"`
 	Fields      []*Field      `{ @@ } "}"`
@@ -60,18 +67,21 @@ type Struct struct {
 }
 
 type Argument struct {
+	Pos  lexer.Position
 	ID   string `@Int ":"`
 	Type *Type  `@@`
 	Name string `@Ident`
 }
 
 type Throw struct {
+	Pos  lexer.Position
 	ID   string `@Int ":"`
 	Type *Type  `@@`
 	Name string `@Ident`
 }
 
 type Method struct {
+	Pos         lexer.Position
 	ReturnType  *Type         `@@`
 	Name        string        `@Ident`
 	Arguments   []*Argument   `"(" [ @@ { "," @@ } ] ")"`
@@ -80,6 +90,7 @@ type Method struct {
 }
 
 type Service struct {
+	Pos         lexer.Position
 	Name        string        `"service" @Ident`
 	Extends     string        `[ "extends" @Ident { @"." @Ident } ]`
 	Methods     []*Method     `"{" { @@ [ ";" ] } "}"`
@@ -88,6 +99,7 @@ type Service struct {
 
 // Literal is a "union" type, where only one matching value will be present.
 type Literal struct {
+	Pos       lexer.Position
 	Str       *string    `  @String`
 	Float     *float64   `| @Float`
 	Int       *int64     `| @Int`
@@ -129,6 +141,7 @@ func (l *Literal) GoString() string {
 }
 
 type MapItem struct {
+	Pos   lexer.Position
 	Key   *Literal `@@ ":"`
 	Value *Literal `@@`
 }
@@ -138,29 +151,34 @@ func (m *MapItem) GoString() string {
 }
 
 type Case struct {
+	Pos         lexer.Position
 	Name        string        `@Ident`
 	Annotations []*Annotation `[ "(" @@ { "," @@ } ")" ]`
 	Value       *Literal      `[ "=" @@ ] [ "," | ";" ]`
 }
 
 type Enum struct {
+	Pos         lexer.Position
 	Name        string        `"enum" @Ident "{"`
 	Cases       []*Case       `{ @@ } "}"`
 	Annotations []*Annotation `[ "(" @@ { "," @@ } ")" ]`
 }
 
 type Typedef struct {
+	Pos  lexer.Position
 	Type *Type  `"typedef" @@`
 	Name string `@Ident`
 }
 
 type Const struct {
+	Pos   lexer.Position
 	Type  *Type    `"const" @@`
 	Name  string   `@Ident`
 	Value *Literal `"=" @@ [ ";" ]`
 }
 
 type Entry struct {
+	Pos        lexer.Position
 	Includes   []string     `  "include" @String`
 	Namespaces []*Namespace `| @@`
 	Structs    []*Struct    `| @@`
@@ -175,6 +193,7 @@ type Entry struct {
 //
 // The grammar
 type Thrift struct {
+	Pos     lexer.Position
 	Entries []*Entry `{ @@ }`
 }
 
