0d1f0a2298f414c0c193f6a7341d050f87b0d5fe
Alec Thomas <alec@swapoff.org>
2018-10-20 20:30:35 -0400

    Modify GraphQL example to parse from files.

--- _examples/graphql/main.go
+++ _examples/graphql/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+
+	"github.com/alecthomas/kong"
 	"github.com/alecthomas/participle"
 	"github.com/alecthomas/repr"
 )
@@ -54,73 +57,23 @@ type Value struct {
 	Symbol string `@Ident`
 }
 
-var parser = participle.MustBuild(&File{})
+var (
+	parser = participle.MustBuild(&File{})
+
+	cli struct {
+		Files []string `arg:"" type:"existingfile" required:"" help:"GraphQL schema files to parse."`
+	}
+)
 
 func main() {
-	ast := &File{}
-	err := parser.ParseString(`
-type Tweet {
-    id: ID!
-    // The tweet text. No more than 140 characters!
-    body: String
-    // When the tweet was published
-    date: Date
-    // Who published the tweet
-    Author: User
-    // Views, retweets, likes, etc
-    Stats: Stat
-}
-
-type User {
-    id: ID!
-    username: String
-    first_name: String
-    last_name: String
-    full_name: String
-    name: String @deprecated
-    avatar_url: Url
-}
-
-type Stat {
-    views: Int
-    likes: Int
-    retweets: Int
-    responses: Int
-}
-
-type Notification {
-    id: ID
-    date: Date
-    type: String
-}
-
-type Meta {
-    count: Int
-}
-
-scalar Url
-scalar Date
-
-type Query {
-    Tweet(id: ID!): Tweet
-    Tweets(limit: Int, skip: Int, sort_field: String, sort_order: String): [Tweet]
-    TweetsMeta: Meta
-    User(id: ID!): User
-    Notifications(limit: Int): [Notification]
-    NotificationsMeta: Meta
-}
-
-type Mutation {
-    createTweet (
-        body: String
-    ): Tweet
-    deleteTweet(id: ID!): Tweet
-    markTweetRead(id: ID!): Boolean
-}
-
-`, ast)
-	if err != nil {
-		panic(err)
+	ctx := kong.Parse(&cli)
+	for _, file := range cli.Files {
+		ast := &File{}
+		r, err := os.Open(file)
+		ctx.FatalIfErrorf(err)
+		err = parser.Parse(r, ast)
+		r.Close()
+		ctx.FatalIfErrorf(err)
+		repr.Println(ast)
 	}
-	repr.Println(ast)
 }
