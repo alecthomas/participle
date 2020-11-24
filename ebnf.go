1734638d47a905fa64aedf3208371560cf9967d7
Alec Thomas <alec@swapoff.org>
2020-11-24 14:58:31 +1100

    Ensure EBNF representation correctly groups sub-expressions.

--- ebnf.go
+++ ebnf.go
@@ -11,7 +11,7 @@ import (
 func (p *Parser) String() string {
 	seen := map[node]bool{}
 	outp := []*ebnfp{}
-	ebnf(p.root, seen, nil, &outp)
+	ebnf(true, p.root, seen, nil, &outp)
 	out := []string{}
 	for _, p := range outp {
 		out = append(out, fmt.Sprintf("%s = %s .", p.name, p.out))
@@ -24,14 +24,20 @@ type ebnfp struct {
 	out  string
 }
 
-func ebnf(n node, seen map[node]bool, p *ebnfp, outp *[]*ebnfp) {
+func ebnf(root bool, n node, seen map[node]bool, p *ebnfp, outp *[]*ebnfp) {
 	switch n := n.(type) {
 	case *disjunction:
+		if !root {
+			p.out += "("
+		}
 		for i, next := range n.nodes {
 			if i > 0 {
 				p.out += " | "
 			}
-			ebnf(next, seen, p, outp)
+			ebnf(false, next, seen, p, outp)
+		}
+		if !root {
+			p.out += ")"
 		}
 		return
 
@@ -46,14 +52,23 @@ func ebnf(n node, seen map[node]bool, p *ebnfp, outp *[]*ebnfp) {
 		seen[n] = true
 		p = &ebnfp{name: name}
 		*outp = append(*outp, p)
-		ebnf(n.expr, seen, p, outp)
+		ebnf(true, n.expr, seen, p, outp)
 		return
 
 	case *sequence:
-		ebnf(n.node, seen, p, outp)
-		if n.next != nil {
-			p.out += " "
-			ebnf(n.next, seen, p, outp)
+		group := n.next != nil && !root
+		if group {
+			p.out += "("
+		}
+		for n != nil {
+			ebnf(false, n.node, seen, p, outp)
+			n = n.next
+			if n != nil {
+				p.out += " "
+			}
+		}
+		if group {
+			p.out += ")"
 		}
 		return
 
@@ -61,46 +76,38 @@ func ebnf(n node, seen map[node]bool, p *ebnfp, outp *[]*ebnfp) {
 		p.out += n.t.Name()
 
 	case *capture:
-		ebnf(n.node, seen, p, outp)
+		ebnf(false, n.node, seen, p, outp)
 
 	case *reference:
 		p.out += "<" + strings.ToLower(n.identifier) + ">"
 
 	case *optional:
-		ebnf(n.node, seen, p, outp)
+		ebnf(false, n.node, seen, p, outp)
 		p.out += "?"
 
 	case *repetition:
-		ebnf(n.node, seen, p, outp)
+		ebnf(false, n.node, seen, p, outp)
 		p.out += "*"
 
 	case *negation:
 		p.out += "!"
-		ebnf(n.node, seen, p, outp)
+		ebnf(false, n.node, seen, p, outp)
 		return
 
 	case *literal:
 		p.out += fmt.Sprintf("%q", n.s)
 
 	case *group:
-		composite := (n.mode != groupMatchOnce) && compositeNode(map[node]bool{}, n, false)
-
-		if composite {
-			p.out += "("
-		}
 		if child, ok := n.expr.(*group); ok && child.mode == groupMatchOnce {
-			ebnf(child.expr, seen, p, outp)
+			ebnf(false, child.expr, seen, p, outp)
 		} else if child, ok := n.expr.(*capture); ok {
 			if grandchild, ok := child.node.(*group); ok && grandchild.mode == groupMatchOnce {
-				ebnf(grandchild.expr, seen, p, outp)
+				ebnf(false, grandchild.expr, seen, p, outp)
 			} else {
-				ebnf(n.expr, seen, p, outp)
+				ebnf(false, n.expr, seen, p, outp)
 			}
 		} else {
-			ebnf(n.expr, seen, p, outp)
-		}
-		if composite {
-			p.out += ")"
+			ebnf(false, n.expr, seen, p, outp)
 		}
 		switch n.mode {
 		case groupMatchNonEmpty:
